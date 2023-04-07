284ffbbf3e8c805ece5f22023cf1dffb1f5b3ba1
Mike <artemjev.mih@gmail.com>
2023-04-08 01:58:17 +0300

    Homework-10, done.

---
+++ src/main/java/com/example/demowithtests/util/mail/springmailer/EmployeeSpringMailer.java
@@ -0,0 +1,30 @@
+package com.example.demowithtests.util.mail.springmailer;
+
+import com.example.demowithtests.domain.Employee;
+import com.example.demowithtests.util.mail.Mailer;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public final class EmployeeSpringMailer implements Mailer {
+
+    private final JavaMailSender mailSender;
+
+    @Override public void send(Employee employee, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo(employee.getEmail());
+        message.setTo("artemjev.mih@gmail.com");
+        message.setSubject(subject + "!!!");
+        message.setText(text);
+
+        mailSender.send(message);
+        log.info("Message sent to employee ({}) successfully", employee);
+        System.err.println("Message sent to employee (" + employee + ") successfully");
+    }
+}
